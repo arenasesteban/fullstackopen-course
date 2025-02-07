ae547abe7b47b9a721d95ef8ec13cde043e0f14b
arenasesteban <esteban.arenas.a@usach.cl>
2025-02-06 23:55:56 -0300

    [Esteban] Add exercise 18 for Part 2

---
+++ part-02/countries-data/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Filter from "./components/Filter";
+import Results from "./components/Results";
+
+function App() {
+    const [countries, setCountries] = useState([]);
+    const [search, setSearch] = useState('');
+
+    const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all';
+
+    useEffect(() => {
+        const fetchCountries = async () => {
+            const response = await axios.get(baseUrl);
+            setCountries(response.data);
+        };
+
+        fetchCountries();
+    }, []);
+
+    const filteredCountries = !search ? countries : countries.filter(country => country.name.common.toLowerCase().includes(search));
+
+    return (
+        <div>
+            <Filter search={search} setSearch={setSearch} />
+            <Results search={search} countries={filteredCountries} />
+        </div>
+    )
+}
+
+export default App;
